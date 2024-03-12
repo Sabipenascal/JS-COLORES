a2dc5b1c292f672ea4421fb5b356cb799a3d34f4
Sabina <sabinakarimova@estudiopenascal.com>
2024-03-12 11:15:02 +0100

    refactor: take from_color and to_color parameters from button element attributes

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 function changeColor(from_color, to_color) {
-    const square = document.getElementById ('square')
-    square.classList.remove (from_color)
-    square.classList.add (to_color)
+    const square = document.getElementById('square')
+    square.classList.remove(from_color)
+    square.classList.add(to_color)
 }
 
-function changeColorToBlue () {
-    changeColor ('red', 'blue')
-}
-function changeColorToRed() {
-    changeColor ('blue','red')
+
+function changeColorToBlue (event) {
+    const button = event.target
+    const from_color = button.attributes.from_color.value
+    const to_color = button.attributes.to_color.value
+    changeColour(from_color, to_color)
 }
 
-const blue_button = document.getElementById ('button')
+function changeColorToRed(event) {
+    const button = event.target
+    const from_color = button.attributes.from_color.value
+    const to_color = button.attributes.to_color.value
+    changeColor(from_color, to_color)
+}
+
+const blue_button = document.getElementById ('blue_button')
 blue_button.onclick = changeColorToBlue
 
 const red_button = document.getElementById ('red_button')
